192beacbcfa79a1d668fa98689c5ec8011c5f319
Vishesh Singh <VisheshSingh16@gmail.com>
2023-03-07 13:40:15 -0500

    make pretty

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import PointsTracker from './components/PointsTracker';
+import TransactionList from './components/TransactionList';
 
 function App() {
   const [transactions, setTransactions] = useState([]);
@@ -59,21 +60,15 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Rewards Program</h1>
+      <div className='main'>
+        <TransactionList transactions={transactions} />
+        <PointsTracker
+          pointsByMonth={pointsByMonth}
+          totalPoints={totalPoints}
+        />
+      </div>
     </div>
   );
 }
